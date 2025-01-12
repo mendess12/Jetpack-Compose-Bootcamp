4e6c5717f6e00250d0e8d2ba7e558919845a961d
yusufmendes <yusufmendes123@gmail.com>
2025-01-12 12:39:59 +0300

    Add color

--- homework-three/app/src/main/java/com/yusufmendes/jetpackcomposehomework3/ui/theme/Color.kt
+++ homework-three/app/src/main/java/com/yusufmendes/jetpackcomposehomework3/ui/theme/Color.kt
@@ -9,3 +9,9 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//project color
+val ScreenBackgroundColor = Color(0xFF382F28)
+val NumberButtonColor = Color(0xFF615751)
+val TextColor = Color(0xFFFFFFFF)
+val MathOperatorButtonColor = Color(0xFFF19A38)

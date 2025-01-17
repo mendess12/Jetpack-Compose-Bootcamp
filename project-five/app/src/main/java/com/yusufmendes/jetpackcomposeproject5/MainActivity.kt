29242a0a3de41767c6343e3d7d48d7a0e8ea5766
yusufmendes <yusufmendes123@gmail.com>
2025-01-17 08:08:53 +0300

    Add project five files

---
+++ project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.yusufmendes.jetpackcomposeproject5
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import com.yusufmendes.jetpackcomposeproject5.ui.theme.JetpackComposeProject5Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            JetpackComposeProject5Theme {
+
+            }
+        }
+    }
+}

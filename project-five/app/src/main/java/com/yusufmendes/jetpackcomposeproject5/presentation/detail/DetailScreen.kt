95793e4a14baba693b61d029d43a504d88ff7639
yusufmendes <yusufmendes123@gmail.com>
2025-01-20 14:26:43 +0300

    Add top bar for screens

---
+++ project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/presentation/detail/DetailScreen.kt
@@ -0,0 +1,54 @@
+package com.yusufmendes.jetpackcomposeproject5.presentation.detail
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen() {
+
+    Scaffold(topBar = {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.LightGray,
+                titleContentColor = Color.Black
+            ),
+            title = {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "User Detail",
+                    textAlign = TextAlign.Center,
+                    fontStyle = FontStyle.Italic,
+                    fontSize = 30.sp,
+                    color = Color.Black
+                )
+            }
+        )
+    }) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
+        ) {
+
+        }
+
+    }
+}

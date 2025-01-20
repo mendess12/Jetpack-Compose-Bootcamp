8dbb97f0d8d04dbbd1352fe036c20cd4c88f48f4
yusufmendes <yusufmendes123@gmail.com>
2025-01-20 15:15:14 +0300

    Add nav args

--- project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/presentation/navigation/NavigateToScreens.kt
+++ project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/presentation/navigation/NavigateToScreens.kt
@@ -1,9 +1,13 @@
 package com.yusufmendes.jetpackcomposeproject5.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.google.gson.Gson
+import com.yusufmendes.jetpackcomposeproject5.data.model.Users
 import com.yusufmendes.jetpackcomposeproject5.presentation.add.AddUserScreen
 import com.yusufmendes.jetpackcomposeproject5.presentation.detail.DetailScreen
 import com.yusufmendes.jetpackcomposeproject5.presentation.home.HomeScreen
@@ -18,8 +22,16 @@ fun NavigateToScreen() {
             HomeScreen(navController = navController)
         }
 
-        composable(route = "detailScreen") {
-            DetailScreen()
+        composable(route = "detailScreen/{user}",
+            arguments = listOf(
+                navArgument("user") {
+                    type = NavType.StringType
+                }
+            )
+        ) {
+            val jsonUser = it.arguments?.getString("user")
+            val user = Gson().fromJson(jsonUser, Users::class.java)
+            DetailScreen(user)
         }
 
         composable(route = "addUserScreen") {

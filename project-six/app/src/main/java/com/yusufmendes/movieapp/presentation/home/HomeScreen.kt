f9a94df908a50a088b3d2c4e57ec1f8d82abd7ed
yusufmendes <yusufmendes123@gmail.com>
2025-01-27 21:20:53 +0300

    Add home screen design

---
+++ project-six/app/src/main/java/com/yusufmendes/movieapp/presentation/home/HomeScreen.kt
@@ -0,0 +1,132 @@
+package com.yusufmendes.movieapp.presentation.home
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import com.yusufmendes.movieapp.data.model.Movies
+
+@SuppressLint("ContextCastToActivity")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(navController: NavController) {
+
+    val movieList = remember { mutableStateListOf<Movies>() }
+    
+
+    LaunchedEffect(key1 = true) {
+        val movie1 = Movies(1, "Django", "django", 24)
+        val movie2 = Movies(1, "Interstellar", "interstellar", 32)
+        val movie3 = Movies(1, "Inception", "inception", 16)
+        val movie4 = Movies(1, "The Hateful Eight", "thehatefuleight", 28)
+        val movie5 = Movies(1, "The Pianist", "thepianist", 18)
+        val movie6 = Movies(1, "Anadoluda", "anadoluda", 10)
+        movieList.add(movie1)
+        movieList.add(movie2)
+        movieList.add(movie3)
+        movieList.add(movie4)
+        movieList.add(movie5)
+        movieList.add(movie6)
+    }
+
+    Scaffold(topBar = {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Yellow,
+                titleContentColor = Color.Black
+            ),
+            title = {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = "Home",
+                    fontSize = 28.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontFamily = FontFamily.SansSerif,
+                    textAlign = TextAlign.Center
+                )
+            }
+        )
+    }) { paddingValues ->
+
+        LazyVerticalGrid(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            columns = GridCells.Fixed(count = 2)
+        ) {
+            items(
+                count = movieList.count(),
+                itemContent = {
+                    val movie = movieList[it]
+                    Card(modifier = Modifier.padding(all = 5.dp)) {
+                        Column(modifier = Modifier.fillMaxWidth()) {
+                            val activity = (LocalContext.current as Activity)
+                            Image(
+                                bitmap = ImageBitmap.imageResource(
+                                    id = activity.resources.getIdentifier(
+                                        movie.resim,
+                                        "drawable",
+                                        activity.packageName
+                                    )
+                                ), contentDescription = "",
+                                modifier = Modifier.size(200.dp, 300.dp)
+                            )
+
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceEvenly
+                            ) {
+                                Text(
+                                    text = "${movie.fiyat} ₺ ",
+                                    fontSize = 24.sp,
+                                    color = Color.Black
+                                )
+                                Button(
+                                    onClick = {}, colors = ButtonDefaults.buttonColors(
+                                        containerColor = Color.Yellow,
+                                        contentColor = Color.Black
+                                    )
+                                ) {
+                                    Text(text = "Sepet")
+                                }
+                            }
+                        }
+                    }
+                }
+            )
+        }
+    }
+}

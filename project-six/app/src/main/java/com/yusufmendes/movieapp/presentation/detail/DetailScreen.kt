f43a40795e240ae931c9441c22f32eb341523353
yusufmendes <yusufmendes123@gmail.com>
2025-01-28 07:43:09 +0300

    Add detail screen design

---
+++ project-six/app/src/main/java/com/yusufmendes/movieapp/presentation/detail/DetailScreen.kt
@@ -0,0 +1,86 @@
+package com.yusufmendes.movieapp.presentation.detail
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yusufmendes.movieapp.data.model.Movies
+
+@SuppressLint("ContextCastToActivity")
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailScreen(getMovie: Movies) {
+
+    Scaffold(topBar = {
+        TopAppBar(
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Yellow,
+                titleContentColor = Color.Black
+            ),
+            title = {
+                Text(
+                    modifier = Modifier.fillMaxWidth(),
+                    text = getMovie.ad,
+                    fontSize = 28.sp,
+                    fontStyle = FontStyle.Italic,
+                    fontFamily = FontFamily.SansSerif,
+                    textAlign = TextAlign.Center
+                )
+            }
+        )
+    }) { paddingValues ->
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.SpaceEvenly,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+
+            val activity = (LocalContext.current as Activity)
+            Image(
+                bitmap = ImageBitmap.imageResource(
+                    id = activity.resources.getIdentifier(
+                        getMovie.resim,
+                        "drawable",
+                        activity.packageName
+                    )
+                ), contentDescription = "",
+                modifier = Modifier.size(200.dp, 300.dp)
+            )
+
+            Text(
+                modifier = Modifier.fillMaxWidth(),
+                text = "${getMovie.fiyat} ₺",
+                color = Color.Black,
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.SansSerif
+            )
+        }
+    }
+}

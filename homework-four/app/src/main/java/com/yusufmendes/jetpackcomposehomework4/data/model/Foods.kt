db1b5a876ab1e247f4d32bc476b6edbf05223ffd
yusufmendes <yusufmendes123@gmail.com>
2025-02-18 18:10:27 +0300

    Add foods model for search screen

--- homework-four/app/src/main/java/com/yusufmendes/jetpackcomposehomework4/data/model/Foods.kt
+++ homework-four/app/src/main/java/com/yusufmendes/jetpackcomposehomework4/data/model/Foods.kt
@@ -3,5 +3,7 @@ package com.yusufmendes.jetpackcomposehomework4.data.model
 data class Foods(
     val id: Int,
     val name: String,
-    val image: String
+    val image: String,
+    val price: Double,
+    val size: String
 )

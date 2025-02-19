f9c8967a6f4c0041e319b094a021e0d56730fa5c
yusufmendes <yusufmendes123@gmail.com>
2025-02-19 20:01:24 +0300

    Update bottom bar screen design

--- homework-four/app/src/main/java/com/yusufmendes/jetpackcomposehomework4/presentation/bottombar/BottomBarScreen.kt
+++ homework-four/app/src/main/java/com/yusufmendes/jetpackcomposehomework4/presentation/bottombar/BottomBarScreen.kt
@@ -7,16 +7,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import com.yusufmendes.jetpackcomposehomework4.R
 import com.yusufmendes.jetpackcomposehomework4.navigation.NavigateToScreen
+import com.yusufmendes.jetpackcomposehomework4.ui.theme.topBarColor
 
 
 @Composable
@@ -24,8 +26,13 @@ fun BottomBarScreen() {
     val selectedItem = remember { mutableStateOf(0) }
     Scaffold(
         bottomBar = {
-            BottomAppBar(content = {
+            BottomAppBar(containerColor = Color.White, content = {
                 NavigationBarItem(
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = topBarColor,
+                        unselectedIconColor = Color.Gray,
+                        indicatorColor = Color.White
+                    ),
                     selected = selectedItem.value == 0,
                     onClick = { selectedItem.value = 0 },
                     icon = {
@@ -33,41 +40,52 @@ fun BottomBarScreen() {
                             painter = painterResource(id = R.drawable.home_icon),
                             contentDescription = ""
                         )
-                    },
-                    label = { Text(text = "Home") }
+                    }
                 )
                 NavigationBarItem(
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = topBarColor,
+                        unselectedIconColor = Color.Gray,
+                        indicatorColor = Color.White
+                    ),
                     selected = selectedItem.value == 1,
                     onClick = { selectedItem.value = 1 },
-                    icon = {
-                        Icon(
-                            painter = painterResource(id = R.drawable.favorite_icon),
-                            contentDescription = ""
-                        )
-                    },
-                    label = { Text(text = "Favorite") }
-                )
-                NavigationBarItem(
-                    selected = selectedItem.value == 2,
-                    onClick = { selectedItem.value = 2 },
                     icon = {
                         Icon(
                             painter = painterResource(id = R.drawable.search_icon),
                             contentDescription = ""
                         )
-                    },
-                    label = { Text(text = "Search") }
+                    }
                 )
                 NavigationBarItem(
-                    selected = selectedItem.value == 3,
-                    onClick = { selectedItem.value = 3 },
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = topBarColor,
+                        unselectedIconColor = Color.Gray,
+                        indicatorColor = Color.White
+                    ),
+                    selected = selectedItem.value == 2,
+                    onClick = { selectedItem.value = 2 },
                     icon = {
                         Icon(
                             painter = painterResource(id = R.drawable.profile_icon),
                             contentDescription = ""
                         )
-                    },
-                    label = { Text(text = "Profile") }
+                    }
+                )
+                NavigationBarItem(
+                    colors = NavigationBarItemDefaults.colors(
+                        selectedIconColor = topBarColor,
+                        unselectedIconColor = Color.Gray,
+                        indicatorColor = Color.White
+                    ),
+                    selected = selectedItem.value == 3,
+                    onClick = { selectedItem.value = 3 },
+                    icon = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.favorite_icon),
+                            contentDescription = ""
+                        )
+                    }
                 )
             })
         }
@@ -80,9 +98,9 @@ fun BottomBarScreen() {
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             if (selectedItem.value == 0) NavigateToScreen(selectScreen = "homeScreen")
-            if (selectedItem.value == 1) NavigateToScreen(selectScreen = "favoritesScreen")
-            if (selectedItem.value == 2) NavigateToScreen(selectScreen = "searchScreen")
-            if (selectedItem.value == 3) NavigateToScreen(selectScreen = "profileScreen")
+            if (selectedItem.value == 1) NavigateToScreen(selectScreen = "searchScreen")
+            if (selectedItem.value == 2) NavigateToScreen(selectScreen = "profileScreen")
+            if (selectedItem.value == 3) NavigateToScreen(selectScreen = "favoritesScreen")
         }
     }
 }

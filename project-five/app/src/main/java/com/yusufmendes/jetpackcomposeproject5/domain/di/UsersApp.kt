a6fc618f2ad2938ed0a215c25cfbeb87b6a20ef7
yusufmendes <yusufmendes123@gmail.com>
2025-03-11 16:20:23 +0300

    Add dagger hilt

---
+++ project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/domain/di/UsersApp.kt
@@ -0,0 +1,8 @@
+package com.yusufmendes.jetpackcomposeproject5.domain.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class UsersApp : Application() {
+}

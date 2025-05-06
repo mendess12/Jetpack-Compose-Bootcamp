aef72da5e8c472170276455c47cf41398c2e80d2
yusufmendes <yusufmendes123@gmail.com>
2025-05-06 20:18:12 +0300

    Add provide user dao

--- project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/domain/di/UsersAppModule.kt
+++ project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/domain/di/UsersAppModule.kt
@@ -1,10 +1,15 @@
 package com.yusufmendes.jetpackcomposeproject5.domain.di
 
+import android.content.Context
+import androidx.room.Room
 import com.yusufmendes.jetpackcomposeproject5.data.repo.UsersRepository
 import com.yusufmendes.jetpackcomposeproject5.data.resource.UsersDataSource
+import com.yusufmendes.jetpackcomposeproject5.room.UsersDao
+import com.yusufmendes.jetpackcomposeproject5.room.UsersDatabase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -14,10 +19,18 @@ object UsersAppModule {
 
     @Provides
     @Singleton
-    fun providesUserDataSource(): UsersDataSource = UsersDataSource()
+    fun providesUserDataSource(usersDao: UsersDao): UsersDataSource = UsersDataSource(usersDao)
 
     @Provides
     @Singleton
     fun provideUserRepository(usersDataSource: UsersDataSource): UsersRepository =
         UsersRepository(usersDataSource)
+
+    @Provides
+    @Singleton
+    fun provideUserDao(@ApplicationContext context: Context): UsersDao {
+        val userDatabase = Room.databaseBuilder(context, UsersDatabase::class.java, "users.sqlite")
+            .createFromAsset("users.sqlite").build()
+        return userDatabase.getUsersDao()
+    }
 }

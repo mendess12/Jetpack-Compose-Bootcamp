75c19aff4d40966d8a1691f8fa4a34707da3392d
yusufmendes <yusufmendes123@gmail.com>
2025-05-06 20:17:02 +0300

    Add room library

--- project-five/app/build.gradle.kts
+++ project-five/app/build.gradle.kts
@@ -73,4 +73,11 @@ dependencies {
     //dagger - hilt
     implementation(libs.hilt.android)
     kapt(libs.hilt.android.compiler)
+
+    //room
+    implementation("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+
 }

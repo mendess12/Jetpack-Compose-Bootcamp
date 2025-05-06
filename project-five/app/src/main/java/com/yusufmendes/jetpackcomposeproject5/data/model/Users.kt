0110678ac9aef8d47048799dc915a7b97757cfd3
yusufmendes <yusufmendes123@gmail.com>
2025-05-06 20:17:28 +0300

    Add entity

--- project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/data/model/Users.kt
+++ project-five/app/src/main/java/com/yusufmendes/jetpackcomposeproject5/data/model/Users.kt
@@ -1,7 +1,13 @@
 package com.yusufmendes.jetpackcomposeproject5.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "kisiler")
 data class Users(
-    val kisi_id: Int,
-    val kisi_ad: String,
-    val kisi_tel: String
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "kisi_id")  val kisi_id: Int,
+    @ColumnInfo(name = "kisi_ad")  val kisi_ad: String,
+    @ColumnInfo(name = "kisi_tel")  val kisi_tel: String
 )
